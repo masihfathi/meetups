0b35d2d716ab8e27c08cfab12de73e35333801c7
masih fathi <masihfathi@gmail.com>
2017-08-12 13:56:30 +0430

    backend completed

--- meetup-backend/server/modules/meetups/routes.js
+++ meetup-backend/server/modules/meetups/routes.js
@@ -2,4 +2,5 @@ import {Router} from 'express';
 import * as MeetupController from './controller';
 const routes = new Router();
 routes.post('/meetups', MeetupController.createMeetup);
-export  default routes;
+routes.get('/meetups', MeetupController.getAllMeetups);
+export default routes;
