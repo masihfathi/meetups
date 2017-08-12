9f23231e87cc0ec13417c704b0c6b55a6b3bdb05
masih fathi <masihfathi@gmail.com>
2017-08-12 17:05:26 +0430

    api complete

--- meetup-mobile/App.js
+++ meetup-mobile/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import {fetchMeetups} from './constants/api';
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    static defaultProps = {
+        fetchMeetups
+    }
+    state = {
+        loading: false,
+        meetups: []
+    }
+    async componentDidMount() {
+        this.setState({
+            loading: true
+        });
+        const data = await this.props.fetchMeetups();
+        this.setState({loading: false, meetups: data.meetups});
+    }
+    render() {
+        if (this.state.loading == true) {
+            return (
+                    <View style={styles.container}>
+                        <ActivityIndicator size="large" color="red"/>
+                    </View>
+                    )
+        }
+        return (
+                <View style={styles.container}>
+                    {this.state.meetups.map((meetup, i) =>
+                        <Text key={i}>{meetup.title}</Text>
+                    )}
+                </View>
+                );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
