cf549d671817070bd399aa7005c99e4b2e2d65f0
Masih Fathi <masihfathi@gmail.com>
2017-08-23 15:37:59 +0430

    first api with axios

--- meetup-mobile/constants/api.js
+++ meetup-mobile/constants/api.js
@@ -5,18 +5,20 @@
 // axios method
 import axios from 'axios';
 axios.defaults.baseURL = 'https://abifiltration.ir/api';
-const FAKEGROUPID = '5997fdd40a6f83293c1d0921';
+const FAKEGROUPID = '5998221e0a6f83293c1d0922';
 class MeetApi {
     constructor() {
         this.groupId = FAKEGROUPID;
         this.path = `/groups/${this.groupId}/meetups`;
     }
-    async fetchGroupMeetups() {
-        const {data} = await axios.get(this.path)
+    async fetchGroupMeetups() {    
+        console.log('masih');
+        console.log('path', this.path);
+        const {data} = await axios.get(this.path);
+        console.log('date', data);
         return data.meetups;
     }
 }
-export {
-    MeetApi
-};
+export default MeetApi;
+
 
--- meetup-mobile/src/screens/home/HomeScreen.js
+++ meetup-mobile/src/screens/home/HomeScreen.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-import {MeetupApi} from '../../../constants/api';
+import MeetupApi from '../../../constants/api';
 import styles from './styles/HomeScreen';
+const MEETUPAPI = new MeetupApi();
 class HomeScreen extends Component {
-    state = {}
+    static defaultProps = {
+        MEETUPAPI
+    }    
+    state = {
+        loading: false,
+        meetups: []
+    }
+    async componentDidMount(){
+        this.setState({
+            loading:true
+        });
+        console.log('majid');
+        const MEETUPS= await this.props.MEETUPAPI.fetchGroupMeetups();
+        this.setState({
+            loading:false,
+            meetups: MEETUPS
+        });
+    }
     render() {
         return (
                 <View style = {styles.root}>
