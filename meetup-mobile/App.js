4d99b584f4a8bb9d664a701bc0fa4fcbc8225e12
Masih Fathi <masihfathi@gmail.com>
2017-08-23 13:11:54 +0430

    first step frontend completed

--- meetup-mobile/App.js
+++ meetup-mobile/App.js
@@ -1,43 +1,12 @@
-import React,{Component} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import {fetchMeetups} from './constants/api';
+import React, {Component} from 'react';
+import  EStyleSheet from 'react-native-extended-stylesheet';
+import Colors from './constants/Colors';
+import  {HomeScreen} from './src/screens'
+EStyleSheet.build(Colors);
 export default class App extends Component {
-    static defaultProps = {
-        fetchMeetups
-    }
-    state = {
-        loading: false,
-        meetups: []
-    }
-    async componentDidMount() {
-        this.setState({
-            loading: true
-        });
-        const data = await this.props.fetchMeetups();
-        console.log('output'+data.meetups);
-        setTimeout( () => this.setState({loading: false, meetups: data.meetups}),2000);
-    }
     render() {
-        if (this.state.loading) {
-            return (
-                    <View style={styles.container}>
-                        <ActivityIndicator size="large" color="red"/>
-                    </View>
-                    );
-        }
         return (
-                <View style={styles.container}>
-                    {this.state.meetups.map( (meetup, i) => <Text key={i}>{meetup.title}</Text> )}
-                </View>
+                <HomeScreen />
                 );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- meetup-mobile/constants/api.js
+++ meetup-mobile/constants/api.js
@@ -1,5 +1,22 @@
-export const fetchMeetups = () => 
-    fetch('https://abifiltration.ir/api/meetups')
-            .then(res => res.json()).catch(e =>console.error('error fetching request'+e));
-
+// fetch method
+/*export const fetchMeetups = () => 
+ fetch('https://abifiltration.ir/api/meetups')
+ .then(res => res.json()).catch(e =>console.error('error fetching request'+e));*/
+// axios method
+import axios from 'axios';
+axios.defaults.baseURL = 'https://abifiltration.ir/api';
+const FAKEGROUPID = '5997fdd40a6f83293c1d0921';
+class MeetApi {
+    constructor() {
+        this.groupId = FAKEGROUPID;
+        this.path = `/groups/${this.groupId}/meetups`;
+    }
+    async fetchGroupMeetups() {
+        const {data} = await axios.get(this.path)
+        return data.meetups;
+    }
+}
+export {
+    MeetApi
+};
 
